2411e171f29e58106cd1f9168ed4384199a1c009
Victor <victor@thoughtstudiosng.com>
2016-02-04 15:54:24 +0100

    initial commit

---
+++ spec/paystack_card_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+require 'paystack/card.rb'
+
+
+describe PaystackCard do
+	it "should be valid" do
+		card = PaystackCard.new(:name => 'Victor Ikoro', :number => '4123450131001381', :cvc => '883', :expiryMonth  => '09', :expiryYear => '19')
+		expect(card.isValidCard).to eq true
+	end
+
+	it "should be invalid" do
+		card = PaystackCard.new(:name => 'Victor Ikoro', :number => '9343450131001381', :cvc => '883', :expiryMonth  => '09', :expiryYear => '19')
+		expect(card.isValidCard).to eq false
+	end
+
+	it "should be invalid" do
+		card = PaystackCard.new(:name => 'Victor Ikoro', :number => '4123450131001381', :cvc => '83', :expiryMonth  => '09', :expiryYear => '12')
+		expect(card.isValidCard).to eq false
+	end
+
+	it "should be invalid" do
+		card = PaystackCard.new(:name => 'Victor Ikoro', :number => '4123450131001381', :cvc => '83', :expiryMonth  => '12', :expiryYear => '18')
+		expect(card.isValidCard).to eq false
+	end
+
+	it "should be invalid" do
+		card = PaystackCard.new(:name => 'Victor Ikoro', :number => '4123450131001381', :cvc => '838', :expiryMonth  => '09', :expiryYear => '12')
+		expect(card.isValidCard).to eq false
+	end
+
+
+end
